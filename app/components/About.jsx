13aaa7ac575aacd4602829e9e69fada9518c3bc2
Nanthakon Worachit <w.nanthakon@gmail.com>
2016-12-07 19:01:55 +0700

    Style About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,15 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About Component!</h3>
+            <h1 className="text-center">About</h1>
             <p>Welcome to About Page!</p>
+            <p>you like weather?</p>
+            <p>i don't know.<br/>
+                but i do.<br/>
+                This website is telling you the temperature everywhere you want.
+                just type location in the search box or a box above the 'get weather' button<br/>
+                have fun!
+            </p>
         </div>
     )
 };
